87dcaab544884dec037dc5d8ab9d42921f866598
Kamil Turek <kamil.turek@hotmail.com>
2022-02-18 22:25:34 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='pyrpm',
+    version='0.1',
+    description='Python RPM package',
+    author='Kamil Turek',
+    license='MIT',
+    packages=['pyrpm'],
+    zip_safe=False,
+)
